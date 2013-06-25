881b031e48bb7dbe9c43061d649100979283f856
d-led <dmlled@yahoo.com>
2013-06-25 23:42:43 +0200

    initial commit

---
+++ premake4.lua
@@ -0,0 +1,72 @@
+local OS=os.get()
+
+local definitions = {
+	dir = {
+		linux = "ls",
+		windows = "dir"
+	}
+}
+
+local cfg={}
+
+for i,v in pairs(definitions) do
+ cfg[i]=definitions[i][OS]
+end
+
+-- Apply to current "filter" (solution/project)
+function DefaultConfig()
+	location "Build"
+	configuration "Debug"
+		defines { "DEBUG", "_DEBUG" }
+		objdir "Build/obj"
+		targetdir "Build/Debug"
+		flags { "Symbols" }
+	configuration "Release"
+		defines { "RELEASE" }
+		objdir "Build/obj"
+		targetdir "Build/Release"
+		flags { "Optimize" }
+	configuration "*" -- to reset configuration filter
+end
+
+function CompilerSpecificConfiguration()
+	configuration {"xcode*" }
+		postbuildcommands {"$TARGET_BUILD_DIR/$TARGET_NAME"}
+
+	configuration {"gmake"}
+		postbuildcommands  { "$(TARGET)" }
+		buildoptions { "-std=gnu++0x" }
+
+	configuration {"codeblocks" }
+		postbuildcommands { "$(TARGET_OUTPUT_FILE)"}
+
+	configuration { "vs*"}
+		postbuildcommands { "\"$(TargetPath)\"" }
+end
+
+-- A solution contains projects, and defines the available configurations
+local sln=solution "lurlutils"
+    location "Build"
+	sln.absbasedir=path.getabsolute(sln.basedir)
+	configurations { "Debug", "Release" }
+	platforms { "native" }
+	includedirs {
+		sln.basedir
+	}
+	vpaths {
+		["Headers"] = "**.h",
+		["Sources"] = {"**.cc", "**.cpp"},
+	}
+
+----------------------------------------------------------------------------------------------------------------
+
+local tests=project "test"
+	local basedir="lurlutil"
+	kind "DynamicLibrary"
+	DefaultConfig()
+	language "C++"
+	files {
+		path.join(basedir,"**.cpp"),
+		path.join(basedir,"**.h")
+	}
+	CompilerSpecificConfiguration()
